0baa3f56fd797a79c349e542db3bfd5d9c8b1333
danielkirwa <kirwadan03@gmail.com>
2021-12-07 17:00:02 +0300

    Added layout for the recycler view

--- app/src/main/java/com/example/smartpoultry/EggProductuon.java
+++ app/src/main/java/com/example/smartpoultry/EggProductuon.java
@@ -3,17 +3,22 @@ package com.example.smartpoultry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class EggProductuon extends AppCompatActivity {
  Spinner chickflockname;
@@ -35,6 +40,61 @@ public class EggProductuon extends AppCompatActivity {
         datecollected = findViewById(R.id.txt_egg_date);
         ChickDatabase chickDatabase = ChickDatabase.getInstance(getApplicationContext());
         chickDao = chickDatabase.chickDao();
+
+
+        // add date picker
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        datecollected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(EggProductuon.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month +1;
+                        String date = day +"/"+month+"/"+year;
+                        datecollected.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+        numberofnottray.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+                int max = 30;
+
+
+                if (numberofnottray.getText().toString().equals("")){
+
+                }else{
+                    if (Integer.parseInt(numberofnottray.getText().toString()) < max){
+
+                    }else{
+                        showMessage("Tray contains 30 eggs","Value should be less than 30");
+                    }
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         // spinner array breads
 
